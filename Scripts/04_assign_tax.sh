ec309c07e1472041ab2c71875a2d379669669127
leonardogirlanda-unifi2026 <leonardo.girlanda@unifi.it>
2026-02-18 16:08:01 +0100

    Update 04_assign_tax.sh

--- Scripts/04_assign_tax.sh
+++ Scripts/04_assign_tax.sh
@@ -1,1 +1,14 @@
+#Taxonomy database download
+crabs db_download -s taxonomy
 
+#Taxonomy assignment
+crabs assign_tax \
+  -i batra_primer_regions410aligned.fasta \
+  -o batra_taxonomy.tsv \
+  -a nucl_gb.accession2taxid \
+  -t nodes.dmp \
+  -n names.dmp \
+  -w yes
+
+#Count sequences per species (species in column 9 of taxonomy output):
+cut -f9 batra_taxonomy.tsv | tail -n +2 | sort | uniq -c | sort -nr
