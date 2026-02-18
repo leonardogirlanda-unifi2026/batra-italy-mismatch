f328451ba8576052a9c38c30635300d7226e00f6
leonardogirlanda-unifi2026 <leonardo.girlanda@unifi.it>
2026-02-18 16:04:04 +0100

    Update 03_fetch_full_sequences.sh

--- Scripts/03_fetch_full_sequences.sh
+++ Scripts/03_fetch_full_sequences.sh
@@ -1,1 +1,2 @@
-
+grep "^>" batra_12S_amplicons.fasta | sed 's/^>//' > batra_amplicons_accessions.txt | efetch -db nuccore -format fasta -id "$(paste -sd, batra_amplicons_accessions.txt)" \
+  > batra_full_sequences.fasta
